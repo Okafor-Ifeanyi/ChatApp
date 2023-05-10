f534f9c79ba1dc550123094ed5d4fdcc634a1ed2
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2023-05-10 02:59:02 +0100

    Feat: Chatroom setup and Authorization with JWT

--- models/chatroom.model.js
+++ models/chatroom.model.js
@@ -4,6 +4,11 @@ const chatroomSchema = new mongoose.Schema({
     name: {
         type: "string",
         required: [true, "Must Have a Name"],  
+    },
+    OwnerID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: "OwnerID needed"
     }
 },
 { timestamp: true }
