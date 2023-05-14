15ac798210620162eba2a43786efeed597d91d04
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2023-05-14 17:51:08 +0100

    feat: Merging react to backend

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ const errorHandlers = require('./handlers/error.handler')
 const app = express();
 require("dotenv").config()
 
+// Setup for JSON files
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Setup Cross-Origin Resource Sharing 
+// to enable passing requests through the frontend
+app.use(require("cors")())
+
+// Setup app routes to call routes 
 app.use("/users", require("./routers/user.router"));
 app.use("/chatroom", require("./routers/chatroom.route"));
 
+// Setup error handler to stop app from breaking
 app.use(errorHandlers.notFound);
-app.use(errorHandlers.mongoseErrors);
+app.use(errorHandlers.mongooseErrors);
 
-// Bringing in the routes
-
+// Using a flow control to specify ENV
 if (process.env.ENV === "DEVELOPMENT") {
     app.use(errorHandlers.developmentErrors); 
 } else {
